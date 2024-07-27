5acd59e500633a5716c2774d0f221a6d76ef09ad
Chaitanya-3012 <chaitanyanevse3012@gmail.com>
2024-07-27 13:27:49 +0530

    Added documentation to JS script file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* This block of code is setting up variables and selecting elements from the HTML document using
+JavaScript. Here's a breakdown of each line: */
 let resetButton = document.querySelector("#reset");
 let newButton = document.querySelector(`#new`);
 let boxes = document.querySelectorAll(".box");
@@ -6,13 +8,23 @@ let bannerMsg = document.querySelector(`.winnerMsg`);
 let banner = document.querySelector(`.winner`);
 banner.style.display = "none";
 
+/**
+ * The `reloader` function in JavaScript reloads the current page.
+ */
 const reloader = () => {
   location.reload();
 };
 
+
+/* The lines `resetButton.addEventListener("click", reloader);` and
+`newButton.addEventListener("click", reloader);` are adding event listeners to the resetButton and
+newButton elements respectively. */
 resetButton.addEventListener("click", reloader);
 newButton.addEventListener("click", reloader);
 
+/* The `const winningPatterns` array in the JavaScript code defines all the possible winning patterns
+in a tic-tac-toe game. Each sub-array within `winningPatterns` represents a combination of positions
+on the game board that would result in a win for a player. */
 const winningPatterns = [
   [1, 2, 3],
   [4, 5, 6],
@@ -24,11 +36,19 @@ const winningPatterns = [
   [3, 5, 7],
 ];
 
+/**
+ * The function `disabler` disables all elements in the `box` array.
+ */
 const disabler = () => {
   for (let boxes of box) {
     box.disabled = true;
   }
 };
+
+/**
+ * The function `checkWinner` iterates through winning patterns in a tic-tac-toe game to check for a
+ * winning combination and display the winner.
+ */
 const checkWinner = () => {
   for (let pattern of winningPatterns) {
     let pos1 = boxes[pattern[0] - 1].innerText;
@@ -49,6 +69,12 @@ const checkWinner = () => {
   }
 };
 
+/**
+ * The function `isDraw` checks if all boxes are disabled and returns true if they are.
+ * @returns The function `isDraw` is checking if all the boxes in the `boxes` array are disabled. If
+ * all boxes are disabled, the function will return `true`, indicating that it is a draw. If at least
+ * one box is not disabled, the function will return `false`, indicating that it is not a draw.
+ */
 const isDraw = () => {
   let disabled;
   for (const box of boxes) {
@@ -65,6 +91,8 @@ const isDraw = () => {
   }
 };
 
+/* The code snippet `boxes.forEach((box) => { ... });` is setting up event listeners for each box
+element in the `boxes` array. */
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (teamO) {
